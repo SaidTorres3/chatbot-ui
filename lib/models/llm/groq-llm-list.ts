0996dab102aca5ecb25e1805cab50596bf3830b3
SaidTorres3 <sesaid17@hotmail.com>
2024-05-03 17:47:04 -0600

    Add LlaMa3_8B and fix Groq LlaMa3_70B pricing

--- lib/models/llm/groq-llm-list.ts
+++ lib/models/llm/groq-llm-list.ts
@@ -17,6 +17,21 @@ const LLaMA2_70B: LLM = {
   }
 }
 
+const LLaMA3_8B: LLM = {
+  modelId: "llama3-8b-8192",
+  modelName: "LLaMA3-8b-chat",
+  provider: "groq",
+  hostedId: "llama3-8b-8192",
+  platformLink: GROQ_PLATORM_LINK,
+  imageInput: false,
+  pricing: {
+    currency: "USD",
+    unit: "1M tokens",
+    inputCost: 0.05,
+    outputCost: 0.1
+  }
+}
+
 const LLaMA3_70B: LLM = {
   modelId: "llama3-70b-8192",
   modelName: "LLaMA3-70b-chat",
@@ -27,8 +42,8 @@ const LLaMA3_70B: LLM = {
   pricing: {
     currency: "USD",
     unit: "1M tokens",
-    inputCost: 0.78,
-    outputCost: 0.9
+    inputCost: 0.59,
+    outputCost: 0.79
   }
 }
 
@@ -47,4 +62,9 @@ const MIXTRAL_8X7B: LLM = {
   }
 }
 
-export const GROQ_LLM_LIST: LLM[] = [LLaMA2_70B, LLaMA3_70B, MIXTRAL_8X7B]
+export const GROQ_LLM_LIST: LLM[] = [
+  LLaMA2_70B,
+  LLaMA3_8B,
+  LLaMA3_70B,
+  MIXTRAL_8X7B
+]
